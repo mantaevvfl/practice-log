52e24f5cbbd2868f03d51935417455286fbed3d0
Ali Mantaev <mantaev.vfl@gmail.com>
2021-02-10 12:52:37 -0500

    Initial Commit

---
+++ Practice Log/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Practice Log
+//
+//  Created by Ali Mantaev on 2021-02-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Practice Log/Practice_LogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_LogApp.swift
+//  Practice Log
+//
+//  Created by Ali Mantaev on 2021-02-10.
+//
+
+import SwiftUI
+
+@main
+struct Practice_LogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
